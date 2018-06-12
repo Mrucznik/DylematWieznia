672ac60acc9e6abd3673201216f4c1b842bd0b15
Mrucznik <mrucznix@gmail.com>
2018-06-12 23:49:17 +0200

    Cumulative strategy abstract and accept apology

---
+++ PrisonerDilema/Strategies/CumulativeStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrisonerDilema.Action;
+
+namespace PrisonerDilema.Strategies
+{
+    abstract class CumulativeStrategy : IStrategy
+    {
+        protected readonly Stack<bool> EnemyMoves = new Stack<bool>(3);
+        protected readonly Stack<bool> MyMoves = new Stack<bool>(3);
+        protected bool LastMove = COOPERATION;
+
+        public abstract bool TakeAction();
+
+        public virtual void ProcessOpponentAction(bool opponentAction)
+        {
+            LastMove = opponentAction;
+            EnemyMoves.Push(opponentAction);
+        }
+    }
+}

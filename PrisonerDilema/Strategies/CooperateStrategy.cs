92978d4978464578d4fbcca32b0b4da6ad5cd520
Mrucznik <mrucznix@gmail.com>
2018-06-13 00:12:20 +0200

    Cooperate strategy

---
+++ PrisonerDilema/Strategies/CooperateStrategy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrisonerDilema.Action;
+
+namespace PrisonerDilema.Strategies
+{
+    class CooperateStrategy : IStrategy
+    {
+        protected bool OponentLastMove = COOPERATION;
+        protected bool MyLastMove;
+
+        public bool TakeAction()
+        {
+            if (MyLastMove == BETRAYAL && OponentLastMove == COOPERATION)
+            {
+                MyLastMove = COOPERATION;
+                return COOPERATION;
+            }
+
+            MyLastMove = OponentLastMove;
+            return OponentLastMove;
+        }
+
+        public void ProcessOpponentAction(bool opponentAction)
+        {
+            OponentLastMove = opponentAction;
+        }
+    }
+}

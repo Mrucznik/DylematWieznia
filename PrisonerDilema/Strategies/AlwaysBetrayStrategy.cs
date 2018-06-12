a042c08147d0c9130ec37dc7cceb6c7b68a387aa
Mrucznik <mrucznix@gmail.com>
2018-06-12 23:01:45 +0200

    Always betray and always cooperate strategions

---
+++ PrisonerDilema/Strategies/AlwaysBetrayStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrisonerDilema.Action;
+
+namespace PrisonerDilema.Strategies
+{
+    class AlwaysBetrayStrategy : IStrategy
+    {
+        public bool TakeAction()
+        {
+            return BETRAYAL;
+        }
+
+        public void ProcessOpponentAction(bool opponentAction)
+        {
+            //do nothing
+        }
+    }
+}

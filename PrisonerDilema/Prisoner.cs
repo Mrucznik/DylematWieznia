b9bc8db4d4a8e58be5579bd47f8e1ebca631e4d7
Mrucznik <mrucznix@gmail.com>
2018-06-12 22:56:24 +0200

    AlxerodStrategy beginning

--- PrisonerDilema/Prisoner.cs
+++ PrisonerDilema/Prisoner.cs
@@ -9,6 +9,7 @@ namespace PrisonerDilema
     class Prisoner : IStrategy
     {
         private IStrategy _strategy;
+        public int Score { get; set; }
 
         public Prisoner(IStrategy strategy)
         {

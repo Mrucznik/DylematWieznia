4762be73ae999bcea232b1cbb932661dbf8b5d45
Mrucznik <mrucznix@gmail.com>
2018-06-12 22:37:23 +0200

    Wet za wet (Tit for tat)

---
+++ PrisonerDilema/Strategies/TitForTat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrisonerDilema.Action;
+
+namespace PrisonerDilema.Strategies
+{
+    class TitForTat : IStrategy
+    {
+        private bool _previousAction = COOPERATION;
+
+        public bool TakeAction()
+        {
+            return _previousAction;
+        }
+
+        public void ProcessOpponentAction(bool opponentAction)
+        {
+            _previousAction = opponentAction;
+        }
+    }
+}
